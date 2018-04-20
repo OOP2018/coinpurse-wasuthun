29318dc5e4560c9823e84d20d6b3e6ac1695dfca
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2018-04-20 14:20:38 +0700

    Generics

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -1,6 +1,7 @@
 package coinpurse;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 /**
@@ -15,9 +16,9 @@ public class MoneyUtil {
  * @param currency
  * @return list of coin with same currency in parameter
  */
-	public static List<Valuable> filterByCurrency(List<Valuable> valuable,String currency){
-		List<Valuable> listcurrency=new ArrayList<>();
-		for (Valuable coin : valuable) {
+	public static <E extends Valuable> List<E> filterByCurrency(List<E> valuable,String currency){
+		List<E> listcurrency=new ArrayList<>();
+		for (E coin : valuable) {
 			if(coin.getCurrency().equals(currency)) {
 				listcurrency.add(coin);
 			}
@@ -28,7 +29,7 @@ public class MoneyUtil {
 	 * This method is use to print by printSort and sort valuable
 	 * @param valuable
 	 */
-	public static void sortCoins(List<Valuable> valuable) {
+	public static void sortMoney(List<?extends Valuable> valuable) {
 		Collections.sort(valuable,new ValueComparator());
 		printSort(valuable);
 	}
@@ -36,7 +37,7 @@ public class MoneyUtil {
 	 * Used print a valuable
 	 * @param valuable
 	 */
-	public static void printSort(List<Valuable> valuable) {
+	public static void printSort(List<?extends Valuable> valuable) {
 		for (Valuable coin : valuable) {
 			System.out.println(coin.toString());
 		}
@@ -55,7 +56,39 @@ public class MoneyUtil {
 		p.insert(new BankNote(50, "Bath",100001));
 		System.out.println(p.toString());
 		System.out.println(filterByCurrency(p.getMoney(), "Bath").toString());
-		sortCoins(p.getMoney());
+		sortMoney(p.getMoney());
+		System.out.println("------");
+		Money m1=new BankNote(100, "Bath", 100002);
+		Money m2=new BankNote(500, "Bath", 100003);
+		Money m3=new Coin(20,"Bath");
+		Money max= MoneyUtil.max(m1,m2,m3);
+		System.out.println(max);
+		String max2=MoneyUtil.max("dog","zebra","cat");
+		System.out.println(max2);
+		List<BankNote> list = new ArrayList<BankNote>();
+		list.add(new BankNote(10, "USD", 1000000));
+		list.add(new BankNote(500, "Bath", 1000001));
+		MoneyUtil.sortMoney(list);
+		List<Coin> coins = Arrays.asList( new Coin(5,"Baht"), new Coin(0.1,"Ringgit"), new Coin(5,"Cent") );
+		List<Coin> result = MoneyUtil.filterByCurrency(coins, "Baht"); 
+		System.out.println(result);		
 		
+		
+	}
+	/**
+	 * Use to find a max of array args
+	 * @param args
+	 * @return max value by use compareTo in E type in args
+	 */
+	public static <E extends Comparable<? super E>> E max(E ... args) throws IllegalArgumentException {
+		if(args.length==0)return null;
+			E max=args[0];
+		for (int i = 0; i < args.length; i++) {
+			if(args[i].compareTo(max)>0) {
+			max=args[i];
+			}
+		}
+		return max;
+	    
 	}
 }
